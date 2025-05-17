48a4f40bf92cd4972294ebb5b68a76c7ab25ab75
PelArtur <pelcharskyi.artur@gmail.com>
2025-05-17 22:38:36 +0300

    add dataset generation

---
+++ Paper_review/conv_tasnet_utils.py
@@ -0,0 +1,74 @@
+"""
+Util classes for Conv TasNet:
+GlobalLayerNorm, CumulativeLayerNorm by JusperLee: https://github.com/JusperLee/Conv-TasNet/tree/master
+"""
+import torch
+import torch.nn as nn
+
+
+class GlobalLayerNorm(nn.Module):
+    '''
+        Calculate Global Layer Normalization
+        dim: (int or list or torch.Size) –
+            input shape from an expected input of size
+        eps: a value added to the denominator for numerical stability.
+        elementwise_affine: a boolean value that when set to True, 
+            this module has learnable per-element affine parameters 
+            initialized to ones (for weights) and zeros (for biases).
+    '''
+
+    def __init__(self, dim, eps=1e-05, elementwise_affine=True):
+        super(GlobalLayerNorm, self).__init__()
+        self.dim = dim
+        self.eps = eps
+        self.elementwise_affine = elementwise_affine
+
+        if self.elementwise_affine:
+            self.weight = nn.Parameter(torch.ones(1, self.dim, 1))
+            self.bias = nn.Parameter(torch.zeros(1, self.dim, 1))
+        else:
+            self.register_parameter('weight', None)
+            self.register_parameter('bias', None)
+
+    def forward(self, x):
+        # x = N x C x L
+        # N x 1 x 1
+        # cln: mean,var N x 1 x L
+        # gln: mean,var N x 1 x 1
+        mean = torch.mean(x, (1, 2), keepdim=True)
+        var = torch.mean((x - mean)**2, (1, 2), keepdim=True)
+        x = (x - mean) / torch.sqrt(var + self.eps)
+        if self.elementwise_affine:
+            x = x * self.weight + self.bias
+        return x
+
+
+class CumulativeLayerNorm(nn.LayerNorm):
+    '''
+       Calculate Cumulative Layer Normalization
+       dim: you want to norm dim
+       elementwise_affine: learnable per-element affine parameters 
+    '''
+
+    def __init__(self, dim, elementwise_affine=True):
+        super(CumulativeLayerNorm, self).__init__(
+            dim, elementwise_affine=elementwise_affine)
+
+    def forward(self, x):
+        # x: N x C x L
+        # N x L x C
+        x = torch.transpose(x, 1, 2)
+        # N x L x C == only channel norm
+        x = super().forward(x)
+        # N x C x L
+        x = torch.transpose(x, 1, 2)
+        return x
+
+
+def select_norm(norm, dim):
+    if norm == 'gln':
+        return GlobalLayerNorm(dim, elementwise_affine=True)
+    if norm == 'cln':
+        return CumulativeLayerNorm(dim, elementwise_affine=True)
+    else:
+        return nn.BatchNorm1d(dim)
